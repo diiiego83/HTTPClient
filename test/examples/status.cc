c42584e1f3bdb2bc259f6ed2b5dbd76d3c01a22e
diiiego83 <moltediego@gmail.com>
2018-05-02 21:52:00 +0100

    comments added

--- test/examples/status.cc
+++ test/examples/status.cc
@@ -11,14 +11,13 @@
 
 using namespace httpclient;
 
-void inline print_line() {
+void inline line() {
     std::cout<<"----------------------------------------------------------------------\n";
 }
 
+// performs a simple http get request
+// raise an exception if a problem occured in the http request
 void client_status() {
-    
-    print_line();
-    std::cout<<" * Raise for client error status, no url set\n";
 
     HTTPOptions opt;
     opt.headers["Accept"] = "application/json";
@@ -33,10 +32,9 @@ void client_status() {
 
 }
 
+// performs a simple http get request
+// raise an exception if the request return an http code != 1xx or != 2xx
 void http_status() {
-
-    print_line();
-    std::cout<<" * Raise for http error status, 500 Internal Server Error\n";
 
     HTTPOptions opt;
     opt.url = "https://httpstat.us/500";
@@ -52,10 +50,11 @@ void http_status() {
 
 }
 
+// performs a simple http get request
+// raise an exception if a problem occured in the http request
+// or if the request return an http code != 1xx or != 2xx
+// in the example no excepion should be thrown
 void client_http_status() {
-
-    print_line();
-    std::cout<<" * Raise if client or http error status, should no raise here\n";
 
     HTTPOptions opt;
     opt.url = "https://httpstat.us/200";
@@ -72,13 +71,14 @@ void client_http_status() {
 
 int main(int argc, char* argv[]) {
 
+    line();
     client_status();
 
+    line();
     http_status();
 
+    line();
     client_http_status();
-
-    print_line();
 
     return 0;
 }
